77d2fae82eecb92277cb35458bfb11ec4d410d82
Ailko <s113170@ap.be>
2020-11-21 12:06:55 +0100

    Wrote Sprite class

--- LostLives/LostLives/Source/Engine/Sprite.cs
+++ LostLives/LostLives/Source/Engine/Sprite.cs
@@ -18,10 +18,13 @@ namespace Lost_Lives
     public class Basic2D
     {
         public Vector2 pos, dims;
+        public Texture2D sprite;
 
-        public Basic2D()
+        public Basic2D(string _path, Vector2 _pos, Vector2 _dims)
         {
-
+            pos = _pos;
+            dims = _dims;
+            sprite = Globals.content.Load<Texture2D>(_path);
         }
 
         public virtual void Update()
@@ -31,7 +34,10 @@ namespace Lost_Lives
 
         public virtual void Draw()
         {
-
+            if(sprite != null)
+            {
+                Globals.spriteBatch.Draw(sprite, new Rectangle((int)pos.X, (int)pos.Y, (int)dims.X, (int)dims.Y), null, Color.White, 0.0f, new Vector2(sprite.Bounds.Width / 2, sprite.Bounds.Height / 2), new SpriteEffects(), 0);
+            }
         }
     }
 }
