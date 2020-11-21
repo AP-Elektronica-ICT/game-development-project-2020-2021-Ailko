f0554b6e2bb116d4d7d34fed3df3819a67b135af
Ailko <s113170@ap.be>
2020-11-21 11:54:45 +0100

    Restarted corrupted project && added base classes

---
+++ LostLives/LostLives/Source/Engine/Basic2D.cs
@@ -0,0 +1,37 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace Lost_Lives
+{
+    public class Basic2D
+    {
+        public Vector2 pos, dims;
+
+        public Basic2D()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
---
+++ LostLives/LostLives/Source/World.cs
@@ -0,0 +1,35 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace Lost_Lives
+{
+    public class World
+    {
+        public World()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
